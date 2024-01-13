3f3d60e5d0d5e85f5a7b0058e47c337756ba664f
kathrynspeltz <kathrynspeltz@gmail.com>
2024-01-12 21:06:38 -0600

    updates

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ export const geo_api_url = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 export const getApiOptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': 'f22dd6575bmshe3376c9a41ba51ep1708b2jsn8a30c912edc8',
+        'X-RapidAPI-Key': 'f22dd6575bmshe3376c9a41ba51ep1708b2jsn8a30c912edc8123',
         'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
     }
 };
